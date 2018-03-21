9c6967671a950287d1a27913947349d11b7f3b72
Ryo Hashiba <Ryo@Ryo-Hashiba-no-MacBook-Pro.local>
2018-03-21 21:40:51 +0900

    restart project for element calculator

---
+++ app/src/main/java/com/example/ryo/aiscalculator/ElementsData.java
@@ -0,0 +1,28 @@
+package com.example.ryo.aiscalculator;
+
+/**
+ * Created by Ryo on 2018/03/21.
+ */
+
+public class ElementsData {
+    int ELEMENT_H = 1;
+    int ELEMENT_HE = 2;
+    int ELEMENT_LI = 3;
+    int ELEMENT_BE = 4;
+    int ELEMENT_B = 5;
+    int ELEMENT_C = 6;
+    int ELEMENT_N = 7;
+    int ELEMENT_O = 8;
+    int ELEMENT_F = 9;
+    int ELEMENT_NE = 10;
+    int ELEMENT_NA = 11;
+    int ELEMENT_MG = 12;
+    int ELEMENT_AL = 13;
+    int ELEMENT_SI = 14;
+    int ELEMENT_P = 15;
+    int ELEMENT_S = 16;
+    int ELEMENT_CL = 17;
+    int ELEMENT_AR = 18;
+    int ELEMENT_K = 19;
+    int ELEMENT_CA = 20;
+}
---
+++ app/src/main/java/com/example/ryo/aiscalculator/RegisterDialog.java
@@ -0,0 +1,11 @@
+package com.example.ryo.aiscalculator;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by Ryo on 2018/03/21.
+ */
+
+public class RegisterDialog extends DialogFragment {
+
+}
